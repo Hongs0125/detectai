ecb525f418bd8cc7811a8745ca24909a65e1156a
Hongs0125 <hsc119119@naver.com>
2023-12-18 18:53:25 +0900

    final

--- detectai/settings.py
+++ detectai/settings.py
@@ -57,18 +57,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-    'refresh',
-    'logintype',
-    'memEmail',
+    '*',
 ]
 
 ROOT_URLCONF = 'detectai.urls'
